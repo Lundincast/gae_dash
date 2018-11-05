a406807e686f0638c12244694965fa09557c641c
lundincast <thecrazybaldhead15@hotmail.com>
2018-11-05 22:24:45 +0100

    Added CloudSQL support

--- gae_dash/gae_dash/settings.py
+++ gae_dash/gae_dash/settings.py
@@ -75,15 +75,47 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+# Check to see if MySQLdb is available; if not, have pymysql masquerade as
+# MySQLdb. This is a convenience feature for developers who cannot install
+# MySQLdb locally; when running in production on Google App Engine Standard
+# Environment, MySQLdb will be used.
+try:
+    import MySQLdb  # noqa: F401
+except ImportError:
+    import pymysql
+    pymysql.install_as_MySQLdb()
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# [START db_setup]
+if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/digaaa-staging:europe-west1:django-rest',
+            'NAME': 'gae_dash',
+            'USER': 'gae-dash',
+            'PASSWORD': 'soh,cah.toa',
+        }
     }
-}
+else:
+    # Running locally so connect to either a local MySQL instance or connect to
+    # Cloud SQL via the proxy. To start the proxy via command line:
+    #
+    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    #
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'gae_dash',
+            'USER': 'gae-dash',
+            'PASSWORD': 'soh,cah.toa',
+        }
+    }
+# [END db_setup]
 
 
 # Password validation
